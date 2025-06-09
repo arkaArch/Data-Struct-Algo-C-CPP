691607b412dd52a38a85a2fe4c3f2a72de6b8e04
arka <arkakar1313@gmail.com>
2025-06-09 16:30:50 +0530

    Modified

--- Interface-vs-Implementation/mymath.cpp
+++
@@ -1,8 +0,0 @@
-// Implementaion
-// Where the work is done
-#include "mymath.hpp"
-
-int add(int a, int b) { return a + b; }
-int sub(int a, int b) { return a - b; }
-int mul(int a, int b) { return a * b; }
-
